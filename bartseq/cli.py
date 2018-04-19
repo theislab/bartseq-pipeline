8264a8a0364b153d310f37a1b87127c80172612b
Philipp A <flying-sheep@web.de>
2018-04-19 09:43:12 +0200

    Show help on 0 args

--- bartseq/cli.py
+++ bartseq/cli.py
@@ -1,5 +1,5 @@
 import sys
-from argparse import ArgumentParser, _SubParsersAction
+from argparse import ArgumentParser, _SubParsersAction, Namespace
 from typing import Sequence, Optional
 
 from .read_tagger.cli import ReadTaggerCLI
@@ -19,5 +19,10 @@ def run_cli(argv: Optional[Sequence[str]] = None):
 	for subcmd in SUBCMDS:
 		subcmd.register_as_subcmd(subparsers)
 	
-	args = parser.parse_args(argv)
-	return args.func(args)
+	args = parser.parse_args(argv)  # type: Namespace
+	if 'func' in args:
+		return args.func(args)
+	elif len(argv) == 0:
+		parser.print_help()
+	else:
+		assert False, 'This should never happen'
